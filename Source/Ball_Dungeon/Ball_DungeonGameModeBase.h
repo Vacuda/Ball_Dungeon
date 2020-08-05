ff7b3640a7e6c6829f054b6029a309362e6d6ecc
Vacuda <adamjsikora@gmail.com>
2020-08-05 12:06:11 -0500

    First Commit

---
+++ Source/Ball_Dungeon/Ball_DungeonGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Ball_DungeonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BALL_DUNGEON_API ABall_DungeonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
